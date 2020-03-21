fc2dff67d0d96cba844b3302b4e962bd5d7fe032
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-21 14:27:07 +0100

    test: chat bot CLI

---
+++ test/call.mjs
@@ -0,0 +1,42 @@
+import got from 'got'
+import cheerio from 'cheerio'
+import prompts from 'prompts'
+
+const webhook = got.extend({
+  prefixUrl: 'http://localhost:3000/api'
+})
+
+function getHook (url) {
+  return `/${url.split('/').pop()}`
+}
+
+function say(actor, message) {
+  return `[${actor}] ${message}
+`
+}
+
+async function call () {
+  let response = await webhook('/start-call').text()
+  let $ = cheerio.load(response)
+
+  while ($('Gather').attr('action')) {
+    let nextAction = getHook($('Gather').attr('action'))
+
+    const { question } = await prompts({
+      type: 'text',
+      name: 'question',
+      message: say('BOT', $('Say').text())
+    });
+
+    response = await webhook.post(nextAction, {
+      json: {
+        SpeechResult: question
+      }
+    }).text()
+
+    $ = cheerio.load(response)
+    console.log($('Say').text())
+  }
+}
+
+call()
