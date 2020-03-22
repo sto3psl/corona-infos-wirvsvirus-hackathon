0eaefe9f4b9f49dc997ac018e013aa9311eafe30
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-22 14:31:38 +0100

    ux(chat): auto scrolling

--- src/index.js
+++ src/index.js
@@ -1,75 +1,91 @@
-import './style';
+import './style'
 import XMLReader from 'xml-reader'
 import XMLQuery from 'xml-query'
 import cx from 'classnames'
-import { h } from 'preact';
-import { useReducer, useEffect } from 'preact/hooks'
+import { h } from 'preact'
+import { useReducer, useEffect, useLayoutEffect } from 'preact/hooks'
 
 function reducer (state, action) {
-	switch (action.type) {
-		case 'input':
-			return {
-				...state,
-				input: action.payload[1],
-				conversation: state.conversation.concat([action.payload])
-			}
-		case 'action':
-			return {
-				...state,
-				action: action.payload
-			}
-		case 'conversation':
-			return {
-				...state,
-				conversation: state.conversation.concat([action.payload])
-			}
-	}
+  switch (action.type) {
+    case 'input':
+      return {
+        ...state,
+        input: action.payload[1],
+        conversation: state.conversation.concat([action.payload])
+      }
+    case 'action':
+      return {
+        ...state,
+        action: action.payload
+      }
+    case 'conversation':
+      return {
+        ...state,
+        conversation: state.conversation.concat([action.payload])
+      }
+  }
 }
 
 export default function App () {
-	const [state, dispatch] = useReducer(reducer, { action: 'start-call', conversation: [] })
+  const [state, dispatch] = useReducer(reducer, {
+    action: 'start-call',
+    conversation: []
+  })
 
-	useEffect(async () => {
-		const response = await (await fetch(`/api/${state.action}`, {
-			method: 'POST',
-			mode: 'cors',
-			headers: {
-				"content-type": "application/json"
-			},
-			body: JSON.stringify({
-				SpeechResult: state.input
-			})
-		})).text()
-		const ast = XMLReader.parseSync(response)
-		const $ = XMLQuery(ast)
-		const text = $.find('Say').map(el => XMLQuery(el).text()).join('\n')
-		const action = $.find('Gather').attr('action')
+  useEffect(async () => {
+    const response = await (
+      await fetch(`http://localhost:3000/api/${state.action}`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          SpeechResult: state.input
+        })
+      })
+    ).text()
+    const ast = XMLReader.parseSync(response)
+    const $ = XMLQuery(ast)
+    const text = $.find('Say')
+      .map(el => XMLQuery(el).text())
+      .join('\n')
+    const action = $.find('Gather').attr('action')
 
-		dispatch({ type: 'conversation', payload: ['BOT', text] })
+    dispatch({ type: 'conversation', payload: ['BOT', text] })
 
-		if (action) {
-			dispatch({ type: 'action', payload: action.split('/').pop() })
-		}
-	}, [state.input])
+    if (action) {
+      dispatch({ type: 'action', payload: action.split('/').pop() })
+    }
+  }, [state.input])
 
-	function handleSubmit (e) {
-		e.preventDefault()
-		const question = new FormData(e.target).get('question')
-		dispatch({ type: 'input', payload: ['CALLER', question] })
-		e.target.reset()
-	}
+  useLayoutEffect(() => {
+    window.scrollTo({
+      top: document.body.getBoundingClientRect().height,
+      behavior: 'smooth'
+    })
+  }, [state.conversation.length])
 
-	return (
-		<div class='conversation'>
-			{state.conversation.map(([actor, entry], i) => {
-				return (
-					<p key={i} class={cx('bubble', actor)}>{entry}</p>
-				)
-			})}
-			<form onSubmit={handleSubmit}>
-				<input name="question" type="text" placeholder="..." />
-				<button type="submit">⬆</button>
-			</form>
-		</div>
-	);
+  function handleSubmit (e) {
+    e.preventDefault()
+    const question = new FormData(e.target).get('question')
+    dispatch({ type: 'input', payload: ['CALLER', question] })
+    e.target.reset()
+  }
+
+  return (
+    <div class='conversation'>
+      {state.conversation.map(([actor, entry], i) => {
+        return (
+          <p key={i} class={cx('bubble', actor)}>
+            {entry}
+          </p>
+        )
+      })}
+      <form onSubmit={handleSubmit}>
+        <input name='question' type='text' placeholder='...' />
+        <button type='submit'>⬆</button>
+      </form>
+    </div>
+  )
 }
